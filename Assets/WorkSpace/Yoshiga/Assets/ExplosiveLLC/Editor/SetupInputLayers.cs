4b995d748deb465649e67a319748963a19bb2c28
leontaicho <92150483+leontaicho@users.noreply.github.com>
2021-12-11 20:33:03 +0900

    aaa

--- Assets/WorkSpace/Yoshiga/Assets/ExplosiveLLC/Editor/SetupInputLayers.cs
+++ Assets/WorkSpace/Yoshiga/Assets/ExplosiveLLC/Editor/SetupInputLayers.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-namespace RPGCharacterAnimsFREE
+namespace RPGCharacterAnims
 {
 	class SetupInputLayers:AssetPostprocessor
 	{
 		static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
 		{
 			SetupMessageWindow window = ( SetupMessageWindow )EditorWindow.GetWindow(typeof(SetupMessageWindow), true, "Load Input and Tag Presets");
-			window.maxSize = new Vector2(330f, 130f);
+			window.maxSize = new Vector2(315f, 130f);
 			window.minSize = window.maxSize;
 		}
 	}
@@ -17,7 +17,7 @@ namespace RPGCharacterAnimsFREE
 	{
 		void OnGUI()
 		{
-			EditorGUILayout.LabelField("Before attempting to use the RPG Animation Mecaim Animation Pack, you must first ensure that the layers and inputs are correctly defined.  There is an included InputManager.preset and LayerManager.preset which contains all the settings that you can load in.\n \nYou can remove this message by deleting the SetupInputLayers.cs script from the Editor folder.", EditorStyles.wordWrappedLabel);
+			EditorGUILayout.LabelField("Before attempting to use the Warrior Animation Mecaim Animation Packs, you must first ensure that the layers and inputs are correctly defined.  There is an included Input.preset and Layer.preset which contains all the settings that you can load in.\n \nYou can remove this message by deleting the SetupInputLayers.cs script in the Editor folder.", EditorStyles.wordWrappedLabel);
 		}
 	}
 }
