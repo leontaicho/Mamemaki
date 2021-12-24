510894dc79d43b6efbd28836304a670c2ed79764
taltupii <92150515+taltupii@users.noreply.github.com>
2021-12-24 16:59:55 +0900

    a

---
+++ Assets/WorkSpace/Nakano/Scripts/BoxAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
+
+public class BoxAction : MonoBehaviour
+{
+    Rigidbody myRB;
+    float Speed = 50;
+
+    public GameObject Target;
+    void Start()
+    {
+        myRB = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        this.transform.LookAt(Target.transform);
+        myRB.velocity = transform.forward * Speed;
+    }
+}
--- Assets/WorkSpace/Nakano/Scripts/EnemAction.cs
+++ Assets/WorkSpace/Nakano/Scripts/EnemAction.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class EnemAction : MonoBehaviour
 {
-    enum State
+    public enum EnemyState
     {
         Idle,
         Walk,
@@ -16,37 +16,53 @@ public class EnemAction : MonoBehaviour
 
     [Header("敵のステータス")]
     [SerializeField]
-    private float Speed = 0;
+    private float Speed = 5;
+    [Header("自身の体力")]
     [SerializeField]
     private float HP;
 
     private Rigidbody myRB;
     private Animator myAnim; // 自身のアニメーション
 
-    private State state;    // 自身のステータス
+    private EnemyState state;    // 自身のステータス
     [Header("土煙 : エフェクト")]
     [SerializeField] private GameObject PatSmoke;
     ParticleSystem.MainModule SmokeMain; //砂煙の本体
     Human_Action[] Human_script = new Human_Action[10];
     public GameObject[] Human_obj;
+    Human_Action PL_Obj;
     GameObject Player_obj; //プレイヤーを取得
-    Human_Action Player;
+    bool isDead = false;
 
-    Transform Target;
+    [Header("死亡後に消えるまでの時間")]
+    [SerializeField]
+    private float deathTime = 3.0f;
+
+    //攻撃した後のフリーズ時間
+    [SerializeField]
+    private float freezeTime = 0.5f;
+
+    [Header("攻撃開始距離")]
+    [SerializeField]
+    float Dis = 3f;
+
     int TargetCount = 0;
 
 
     void Start()
     {
-        for(int i = 0; i < Human_obj.Length; ++i)
+
+        state = EnemyState.Walk;
+        for (int i = 0; i < Human_obj.Length; ++i)
         {
             Human_script[i] = Human_obj[i].GetComponent<Human_Action>();
         }
 
+        TargetCount = 0;
+
         //プレイヤーを取得
         Player_obj = GameObject.FindGameObjectWithTag("Player");
-        Player = Player_obj.GetComponent<Human_Action>();
-
+        PL_Obj = Player_obj.GetComponent<Human_Action>();
         //自身のRigidbodyを取得
         myRB = this.gameObject.GetComponent<Rigidbody>();
         //自身のアニメーションを取得
@@ -56,36 +72,82 @@ public class EnemAction : MonoBehaviour
     }
 
 
-    void EnemyController()
+    private void OnTriggerEnter(Collider other)
     {
-
-        if(Human_script[TargetCount].isDaed == true)
+        if (other.gameObject.tag == "club" && !isDead)
         {
-            TargetCount++;
+            if (HP < 0)
+            {
+                Destroy(gameObject, deathTime);
+            }
+            else
+            {
+                HP--;
+            }
+
+        }
+    }
+
+    void Attack(Transform Pos)
+    {
+        float dis = Vector3.Distance(this.gameObject.transform.position, Pos.transform.position);
+        Pos.transform.LookAt(PL_Obj.transform.position);
+        myRB.velocity = transform.forward * Speed;
+        myAnim.SetFloat("Speed", Speed);
+        if (Dis > dis)
+        {
+            myAnim.SetFloat("Speed", 0);
+            myRB.velocity = Vector3.zero;
+            myAnim.SetBool("Attack", true); //攻撃開始
         }
 
-        else if(Player.isDaed == true)
-        {
-            Target = Player.gameObject.transform;
-            transform.LookAt(Target, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z));
-            myRB.velocity = new Vector3() + transform.forward * Speed;
-        }
-
-        else 
-        {
-            Target = Human_script[TargetCount].gameObject.transform;
-            transform.LookAt(Target, new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z));
-            myRB.velocity = new Vector3() + transform.forward * Speed;
-        }
-
+        
+      
     }
 
 
 
-    void FIxedUpdate()
+    void Update()
     {
 
-        EnemyController();
+        //switch (state)
+        //{
+        //    case EnemyState.Walk:
+
+        //        break;
+
+        //    case EnemyState.Hit:
+        //        break;
+
+        //    case EnemyState.Attack:
+        //        break;
+
+        //    default:
+        //        break;
+        //}
+
+
+
+        if (Human_script[TargetCount].isDaed == true)
+        {
+            TargetCount++;
+        }
+
+        else if (PL_Obj.isDaed == true)
+        {
+            //回転値を取得
+            this.transform.LookAt(PL_Obj.transform.position);
+            myRB.velocity = transform.forward * Speed;
+            //    myRB.velocity = new Vector3(transform.position.x, 0, transform.position.z) + transform.forward * Speed;
+        }
+
+        else
+        {
+            //回転値を取得
+            this.transform.LookAt(Human_obj[TargetCount].gameObject.transform.position);
+            myRB.velocity = transform.forward * Speed;
+            myAnim.SetFloat("Speed", Speed);
+        }
 
     }
 }
