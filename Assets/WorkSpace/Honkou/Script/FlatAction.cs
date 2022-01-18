c0bf8d1cc2a5bb11629f95638bfee3bfd6a04c72
m-honko0819 <92153002+m-honko0819@users.noreply.github.com>
2022-01-18 19:06:41 +0900

    Add Shader

---
+++ Assets/WorkSpace/Honkou/Script/FlatAction.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlatAction: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    void FlatShading()
+    {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = Instantiate(mf.sharedMesh) as Mesh;
+        mf.sharedMesh = mesh;
+
+        Vector3[] oldVerts = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        Vector3[] vertices = new Vector3[triangles.Length];
+
+        for(int i = 0; i < triangles.Length; i++)
+        {
+            vertices[i] = oldVerts[triangles[i]];
+            triangles[i] = i;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+/*        //==　デバッグ用　==//
+        if(Input.GetMouseButtonDown(0))
+        {
+*/            FlatShading();
+/*        }
+*/    }
+}
