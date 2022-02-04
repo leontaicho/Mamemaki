d24e631506fd4eac841cbb86758702e7b9d9bfda
m-honko0819 <92153002+m-honko0819@users.noreply.github.com>
2022-02-04 18:04:42 +0900

    Add Title

---
+++ Assets/WorkSpace/Honkou/Script/TitleAction.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleAction : MonoBehaviour
+{
+    enum TitleState
+    {
+        Init,
+        Fade,
+        Logo,
+        LogoFade,
+        Menu,
+        Tutorial,
+        Load,
+    };
+    TitleState state;
+
+    public Image Fade;
+    public GameObject Logo;
+    public GameObject Menu;
+    public Image Menu_1;
+    public Image Menu_12;
+    public Image Menu_2;
+    public Image Menu_22;
+    Animator FadeLogo;
+    Animator FadeMenu;
+
+    float Elapsed = 0.0f;
+    float Alpha = 1.0f;
+    int Select = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeLogo = Logo.gameObject.GetComponent<Animator>();
+        FadeMenu = Menu.gameObject.GetComponent<Animator>();
+
+        TitleInit();
+    }
+
+    void TitleInit()
+    {
+        Fade.gameObject.SetActive(true);
+        Logo.gameObject.SetActive(true);
+        Menu.gameObject.SetActive(false);
+
+        //Logo.transform.position = new Vector3(0, 0, 0);
+        state = TitleState.Fade;
+        Elapsed = 0.0f;
+        Alpha = 1.0f;
+        Select = 0;
+    }
+
+    void FadeLogoFinish()
+    {
+        Fade.gameObject.SetActive(false);
+        Logo.gameObject.SetActive(false);
+        Menu.gameObject.SetActive(true);
+
+        Menu_1.gameObject.SetActive(false);
+        Menu_12.gameObject.SetActive(true);
+        Menu_2.gameObject.SetActive(false);
+        Menu_22.gameObject.SetActive(true);
+
+        FadeMenu.SetTrigger("FadeMenu");
+    }
+
+    void FadeMenuFinish()
+    {
+        state = TitleState.Menu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(state)
+        {
+            case TitleState.Init:
+                TitleInit();
+                break;
+
+            case TitleState.Fade:
+
+                Alpha -= 0.005f;
+                Fade.color = new Color(1.0f, 1.0f, 1.0f, Alpha);
+
+                if(Alpha <= 0)
+                {
+                    state = TitleState.Logo;
+                    Alpha = 0;
+                }
+                break;
+
+            case TitleState.Logo:
+                if(Input.GetButtonDown("BtnA"))
+                {
+                    state = TitleState.LogoFade;
+                }
+                break;
+
+            case TitleState.LogoFade:
+                FadeLogo.SetTrigger("FadeLogo");
+                Invoke("FadeLogoFinish", 5.1f);
+                Invoke("FadeMenuFinish", 1.4f);
+                break;
+
+            case TitleState.Menu:
+                if(Input.GetAxis("HorizontalPad") <= -1)
+                {
+                    Select = 0;
+                    Menu_1.gameObject.SetActive(true);
+                    Menu_12.gameObject.SetActive(false);
+                    Menu_2.gameObject.SetActive(false);
+                    Menu_22.gameObject.SetActive(true);
+                }
+                else if(Input.GetAxis("HorizontalPad") >= 1)
+                {
+                    Select = 1;
+                    Menu_1.gameObject.SetActive(false);
+                    Menu_12.gameObject.SetActive(true);
+                    Menu_2.gameObject.SetActive(true);
+                    Menu_22.gameObject.SetActive(false);
+                }
+
+                if (Input.GetButtonDown("BtnA"))
+                {
+                    switch(Select)
+                    {
+                        case 0:
+                            Fade.gameObject.SetActive(true);
+                            state = TitleState.Load;
+                            break;
+
+                        case 1:
+                            state = TitleState.Tutorial;
+                            break;
+                    }
+                }
+                break;
+
+            case TitleState.Tutorial:
+                if (Input.GetButtonDown("BtnA"))
+                {
+                    state = TitleState.Menu;
+                }
+                break;
+
+            case TitleState.Load:
+                Fade.color = new Color(1.0f, 1.0f, 1.0f, Alpha);
+                Alpha += 0.005f;
+                if (Alpha >= 1)
+                {
+                    state = TitleState.Init;
+                }
+                break;
+        }
+    }
+}
